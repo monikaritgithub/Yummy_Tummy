6ab9f1781c41689c497c7d7d0f21134b92316d2d
Smriti Bhandari <moonekabhandari123@gmail.com>
2024-02-02 07:50:18 -0900

    log sheet 6 admin navbar

---
+++ LOG_SHEET_6_ADMIN_NAVBAR/LOG_SHEET_6_ADMIN_NAVBAR/resources/views/customer/customerDashboard.blade.php
@@ -0,0 +1,20 @@
+<x-guest-layout>
+
+    <x-slot name="header">
+
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    Welcome customer, {{ auth()->user()->name }}
+</h2>
+<x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-nav-link>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome />
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
